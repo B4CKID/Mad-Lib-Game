23581f7669cb330a278508c72491412757e43e0c
Billy Barnes <86270229+B4CKID@users.noreply.github.com>
2021-08-31 20:39:26 +0000

    Menu now added

--- Mad_Lib_Generator/Mad_Libs.py
+++ Mad_Lib_Generator/Mad_Libs.py
@@ -7,20 +7,29 @@ class MadLibs:
     return None
 
   def __repr__(self):
-    return f' Here is your new madlib! {new_libs}'
+    return new_libs
 
-def mad_lib1():
-  first = input("Enter first word: ")
-  second = input("Enter second word: ")
-  third = input("Enter Third word: ")
-  fourth = input("Enter Fouth word: ")
-  fifth = input("Enter Fifth word: ")
-  for libs in mlb1:# Maybe use if statements
-    new_libs = libs.replace("1", first)
-    new_libs = new_libs.replace("2",second)
-    new_libs = new_libs.replace ("3", third)
-    new_libs = new_libs.replace ("4", fourth)
-    new_libs = new_libs.replace ("5", fifth)
-    print(new_libs)
+  def mad_lib1():
+    first = input("Enter first word(This is should be an adjective): ")
+    second = input("Enter second word (This is should be a plural nonun): ")
+    third = input("Enter Third word (This is should be a noun): ")
+    fourth = input("Enter Fouth word (This is should be an adverb): ")
+    fifth = input("Enter Fifth word (This is should be a number): ")
+    sixth = input("Enter sixth word This is should be an past tense verb): ")
+    seventh = input("Enter seventh word (This is should be an -est adjective): ")
+    eighth = input("Enter eighth word (This is should be an past tense verb): ")
+    ninth = input("Enter ninth word (This is should be an adverb): ")
+    tenth = input("Enter tenth word (This is should be an adjective): ")
+    for libs in mlb1:# Maybe use if statements
+      new_libs = libs.replace("1", first)
+      new_libs = new_libs.replace("2",second)
+      new_libs = new_libs.replace ("3", third)
+      new_libs = new_libs.replace ("4", fourth)
+      new_libs = new_libs.replace ("5", fifth)
+      new_libs = new_libs.replace ("6", sixth)
+      new_libs = new_libs.replace ("7", seventh)
+      new_libs = new_libs.replace ("8", eighth)
+      new_libs = new_libs.replace ("9", ninth)
+      new_libs = new_libs.replace ("10", tenth)
+      print(new_libs)
 
-mad_lib1()
--- Menu.py
+++ Menu.py
@@ -1,4 +1,18 @@
-first = input("Enter first word here")
-second = input("Enter second word here")
+import time
+import main
 
-While first != q:
+from Mad_Lib_Generator.Mad_Libs import MadLibs
+
+print("Welcome to Madlib and Sadlibs")
+time.sleep(2)
+user_input = input('Please choose your Madlib Category')
+while user_input != q:
+  if user_input == 1:
+    mad_lib1()
+  
+  elif user_input = q
+    print('Terminating program')
+      break
+
+  else print('Not a valid choice!')  
+  
