559d9238e5c88633d7bc739a665dda07e19783d3
Billy Barnes <86270229+B4CKID@users.noreply.github.com>
2021-08-31 19:22:56 +0000

    Adding to the app

--- Mad_Lib_Generator/Mad_Libs.py
+++ Mad_Lib_Generator/Mad_Libs.py
@@ -1,20 +1,26 @@
-mlb1 =[ 'This is the 1 madlib to 2 its fucntionality'
-]
+from Mad_Lib_Storage.Madlib_Text import mlb1
+
 
 class MadLibs:
 
   def __init__(self):
     return None
 
-  def __repr__(self, Madlib_Number):
+  def __repr__(self):
     return f' Here is your new madlib! {new_libs}'
 
 def mad_lib1():
   first = input("Enter first word: ")
   second = input("Enter second word: ")
+  third = input("Enter Third word: ")
+  fourth = input("Enter Fouth word: ")
+  fifth = input("Enter Fifth word: ")
   for libs in mlb1:# Maybe use if statements
     new_libs = libs.replace("1", first)
     new_libs = new_libs.replace("2",second)
+    new_libs = new_libs.replace ("3", third)
+    new_libs = new_libs.replace ("4", fourth)
+    new_libs = new_libs.replace ("5", fifth)
     print(new_libs)
 
 mad_lib1()
---
+++ Mad_Lib_Storage/Madlib_Text.py
@@ -0,0 +1,2 @@
+mlb1 = [ 'This is the 1 madlib to 2 its fucntionality. I also wanted 3 for the next example. Just 4 is enough for the other thing, and dont like too much 5'
+]
